9d9f23191f4203cde0a4799d25d2b682f3403413
Gregor Sturm <mail@gregor-sturm.de>
2021-07-08 14:26:44 +0200

    Update QC pipeline

--- analyses/20_qc_norm_scrnaseq_data/run_pipeline.sh
+++ analyses/20_qc_norm_scrnaseq_data/run_pipeline.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
 cd ../.. && \
-nextflow run /home/sturm/projects/2020/single-cell-analysis-nf/main.nf \
+nextflow run ./single-cell-analysis-nf/main.nf \
     --input tables/samplesheet_scrnaseq_preprocessing.csv \
     --outdir data/20_qc_norm_scrnaseq \
     -resume \
-    -profile icbi_long \
-    -w /data/scratch/sturm/projects/2020/pircher-scrnaseq-lung
-
+    -profile icbi \
+    -w /data/scratch/sturm/projects/2020/pircher-scrnaseq-lung \
+    --publish_dir_mode=link \
+    --skip_solo
