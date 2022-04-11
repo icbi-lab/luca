4fd40f0211026c4c8fca6fc22fb64c10d7c94bb0
Gregor Sturm <mail@gregor-sturm.de>
2022-04-11 16:19:31 +0200

    update pack script

--- pack_for_zenodo.sh
+++ pack_for_zenodo.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # # singularity containers needed to run all processes in the workflow
-# tar cvf - --dereference --hard-dereference containers | pigz -p 32 > data/zenodo/containers.tar.gz
 tar cvf - --dereference --hard-dereference containers | xz -9 -T 32 > data/zenodo/containers.tar.xz
 
 # # input data required to run all workflows from scratch
-# tar cvf - --dereference --hard-dereference data/13_tcga/for_scissor data/12_input_adatas data/11_own_datasets/{batch1_3patients,batch2_5patients,velocyto} | pigz -p 32 > data/zenodo/input_data.tar.gz
 tar cvf - --dereference --hard-dereference data/13_tcga/for_scissor data/12_input_adatas data/11_own_datasets/{batch1_3patients,batch2_5patients,velocyto} | xz -9 -T 32 > data/zenodo/input_data.tar.xz
 
 # intermediate results (required to run the second part of the workflow that doesn't depend on specialized hardware)
