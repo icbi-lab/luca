885d309fe91f1378ac4926a6adc561281e029ad8
Gregor Sturm <mail@gregor-sturm.de>
2022-04-12 14:41:51 +0200

    Fully containerize build_atlas workflow

--- pack_for_zenodo.sh
+++ pack_for_zenodo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # # singularity containers needed to run all processes in the workflow
-tar cvf - --dereference --hard-dereference containers | xz -9 -T 32 > data/zenodo/containers.tar.xz
+# tar cvf - --dereference --hard-dereference containers | xz -9 -T 32 > data/zenodo/containers.tar.xz
 
 # # input data required to run all workflows from scratch
-tar cvf - --dereference --hard-dereference data/13_tcga/for_scissor data/12_input_adatas data/11_own_datasets/{batch1_3patients,batch2_5patients,velocyto} | xz -9 -T 32 > data/zenodo/input_data.tar.xz
+# tar cvf - --dereference --hard-dereference data/13_tcga/for_scissor data/12_input_adatas data/11_own_datasets/{batch1_3patients,batch2_5patients,velocyto} | xz -9 -T 32 > data/zenodo/input_data.tar.xz
 
 # intermediate results (required to run the second part of the workflow that doesn't depend on specialized hardware)
-# tar cvf - --dereference --hard-dereference data/20_build_atlas | pigz -p 32 > data/zenodo/build_atlas_results.tar.gz
+tar cvf - --dereference --hard-dereference data/20_build_atlas | pigz -p 32 > data/zenodo/build_atlas_results.tar.gz
 
 # final results (all results produced by the second part of the workflow)
 # tar cvf - --dereference --hard-dereference data/30_downstream_analyses | pigz -p 32 > data/zenodo/downstream_analyses_results.tar.gz
