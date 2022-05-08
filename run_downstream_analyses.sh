7ef3daa2cdaf34a2f7a199dc692a3bf9878e30ce
Gregor Sturm <mail@gregor-sturm.de>
2022-05-08 11:40:43 +0200

    Use singularity containers for everything

--- run_downstream_analyses.sh
+++ run_downstream_analyses.sh
@@ -4,6 +4,5 @@ nextflow run main.nf --workflow downstream_analyses \
     --atlas "./data/20_build_atlas/add_additional_datasets/03_update_annotation/artifacts/full_atlas_merged.h5ad" \
     --outdir ./data/30_downstream_analyses \
     -resume \
-    -profile icbi \
-    --publish_dir_mode link \
+    -profile icbi_lung \
     -w /data/scratch/sturm/projects/2020/pircher-scrnaseq-lung/downstream
