888ef4b6a0d5cdc0a19fc96c0ade4456efe6f350
Gregor Sturm <mail@gregor-sturm.de>
2022-05-08 11:38:30 +0200

    Pack input data and build_atlas data  for zenodo

--- pack_for_zenodo.sh
+++ pack_for_zenodo.sh
@@ -4,11 +4,14 @@
 # tar cvf - --dereference --hard-dereference containers | xz -9 -T 32 > data/zenodo/containers.tar.xz
 
 # # input data required to run all workflows from scratch
-# tar cvf - --dereference --hard-dereference data/13_tcga/for_scissor data/12_input_adatas data/11_own_datasets/{batch1_3patients,batch2_5patients,velocyto} | xz -9 -T 32 > data/zenodo/input_data.tar.xz
+tar cvf - --dereference --hard-dereference data/10_references data/13_tcga/for_scissor data/12_input_adatas data/11_own_datasets/{batch1_3patients,batch2_5patients,velocyto} | xz -9 -T 32 > data/zenodo/input_data.tar.xz
 
 # intermediate results (required to run the second part of the workflow that doesn't depend on specialized hardware)
-tar cvf - --dereference --hard-dereference data/20_build_atlas | pigz -p 32 > data/zenodo/build_atlas_results.tar.gz
+# tar cvf - --dereference --hard-dereference data/20_build_atlas | xz -9 -T 32 > data/zenodo/build_atlas_results.tar.xz
 
 # final results (all results produced by the second part of the workflow)
 # tar cvf - --dereference --hard-dereference data/30_downstream_analyses | pigz -p 32 > data/zenodo/downstream_analyses_results.tar.gz
 # tar cvf - --dereference --hard-dereference data/30_downstream_analyses | xz -9 -T 32 > data/zenodo/downstream_analyses_results.tar.xz
+
+# copy the core atlas, extended atlas and scArches model as separate files
+# TODO
