c0cf06a6c8f4af3c843c8eb2509ba208199898e6
Gregor Sturm <mail@gregor-sturm.de>
2022-05-10 19:45:11 +0200

    Update zenodo pack script

--- pack_for_zenodo.sh
+++ pack_for_zenodo.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 # # singularity containers needed to run all processes in the workflow
-tar cvf - --dereference --hard-dereference containers | xz -9 -T 32 > data/zenodo/containers.tar.xz
+tar cvf - --dereference containers | pigz -p 32 > data/zenodo/containers.tar.gz
 
 # # input data required to run all workflows from scratch
-# tar cvf - --dereference --hard-dereference data/10_references data/13_tcga/for_scissor data/12_input_adatas data/11_own_datasets/{batch1_3patients,batch2_5patients,velocyto} | xz -9 -T 32 > data/zenodo/input_data.tar.xz
+tar cvf - --dereference data/10_references data/13_tcga/for_scissor data/12_input_adatas data/11_own_datasets/{batch1_3patients,batch2_5patients,velocyto} | pigz -p 32 > data/zenodo/input_data.tar.gz
 
 # intermediate results (required to run the second part of the workflow that doesn't depend on specialized hardware)
-# tar cvf - --dereference --hard-dereference data/20_build_atlas | xz -9 -T 32 > data/zenodo/build_atlas_results.tar.xz
+tar cvf - --dereference data/20_build_atlas | pigz -p 32 > data/zenodo/build_atlas_results.tar.gz
 
 # final results (all results produced by the second part of the workflow)
-# tar cvf - --dereference --hard-dereference data/30_downstream_analyses | pigz -p 32 > data/zenodo/downstream_analyses_results.tar.gz
-# tar cvf - --dereference --hard-dereference data/30_downstream_analyses | xz -9 -T 32 > data/zenodo/downstream_analyses_results.tar.xz
+tar cvf - --dereference data/30_downstream_analyses | pigz -p 32 > data/zenodo/downstream_analyses_results.tar.gz
 
 # copy the core atlas, extended atlas and scArches model as separate files
-# TODO
+tar cvf - --dereference  \
+  data/20_build_atlas/annotate_datasets/35_final_atlas/full_atlas_hvg_integrated_scvi_scanvi_model \
+  data/20_build_atlas/annotate_datasets/35_final_atlas/full_atlas_hvg_integrated_scvi_integrated_scanvi.h5ad | \
+  pigz -p 32 > data/zenodo/core_atlas_scanvi_model.tar.gz
+
+pigz -c -p 32 data/20_build_atlas/annotate_datasets/35_final_atlas/artifacts/full_atlas_annotated.h5ad > data/zenodo/core_atlas.h5ad.gz
+
+pigz -c -p 32 data/20_build_atlas/add_additional_datasets/03_update_annotation/artifacts/full_atlas_merged.h5ad > data/zenodo/extended_atlas.h5ad.gz
+
+
