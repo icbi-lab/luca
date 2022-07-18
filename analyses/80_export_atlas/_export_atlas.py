29b55b2dc7942fb2f0e9af1c468406fda7b865e9
Gregor Sturm <mail@gregor-sturm.de>
2022-07-18 15:57:13 +0200

    Add stub notebook for exporting final atlas

---
+++ analyses/80_export_atlas/_export_atlas.py
@@ -0,0 +1,23 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.1
+#   kernelspec:
+#     display_name: Python [conda env:.conda-pircher-sc-integrate2]
+#     language: python
+#     name: conda-env-.conda-pircher-sc-integrate2-py
+# ---
+
+# %% [markdown]
+# Export final atlas for sharing. 
+#  * Use ontologies
+#  * Remove unnecessary data
+#  * make sure it passes the cellxgene schema
+#  * output documentation
+
+# %%
+# TODO
