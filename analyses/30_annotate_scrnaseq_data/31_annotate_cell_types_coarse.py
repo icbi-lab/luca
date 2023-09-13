fb99ede4ad74b9f787ec7bb9d093512d0baf99bd
nictru <nictru32@gmail.com>
2023-09-13 11:32:48 +0200

    Implement cell type annotation using celltypist

--- analyses/30_annotate_scrnaseq_data/31_annotate_cell_types_coarse.py
+++ analyses/30_annotate_scrnaseq_data/31_annotate_cell_types_coarse.py
@@ -18,28 +18,53 @@
 
 # %%
 import scanpy as sc
-from scanpy_helpers.annotation import AnnotationHelper
 import warnings
 import numpy as np
 from nxfvars import nxfvars
+import celltypist
+from celltypist import models
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 # %%
 sc.set_figure_params(figsize=(5, 5))
 
-# %%
-ah = AnnotationHelper()
 
 # %%
-input_dir = nxfvars.get(
-    "input_dir",
-    "../../data/20_integrate_scrnaseq_data/integrate_datasets/27_leiden_umap_nodoublet/",
-)
 artifact_dir = nxfvars.get("artifact_dir", "/home/sturm/Downloads")
 
 # %%
-adata = sc.read_h5ad(f"{input_dir}/adata.doublet_filtered.umap_leiden.h5ad")
+adata = sc.read_h5ad(f"adata.doublet_filtered.umap_leiden.h5ad")
+
+# %%
+adata_celltypist = adata.copy()  # make a copy of our adata
+adata_celltypist.X = adata.layers["raw_counts"]  # set adata.X to raw counts
+sc.pp.normalize_per_cell(
+    adata_celltypist, counts_per_cell_after=10**4
+)  # normalize to 10,000 counts per cell
+sc.pp.log1p(adata_celltypist)  # log-transform
+# make .X dense instead of sparse, for compatibility with celltypist:
+adata_celltypist.X = adata_celltypist.X.toarray()
+
+# %%
+models.download_models(model="Cells_Intestinal_Tract.pkl")
+model = models.Model.load("Cells_Intestinal_Tract.pkl")
+
+# %% 
+predictions = celltypist.annotate(
+    adata_celltypist, model=model, majority_voting=True
+)
+predictions_adata = predictions.to_adata()
+predictions_adata
+
+# %%
+adata.obs["celltypist_prediction"] = predictions_adata.obs.loc[
+    adata.obs.index, "majority_voting"
+]
+
+adata.obs["celltypist_conf_score"] = predictions_adata.obs.loc[
+    adata.obs.index, "conf_score"
+]
 
 # %%
 adata.obs["leiden"] = adata.obs["leiden_1.00"]
@@ -51,16 +76,7 @@ sc.pl.umap(adata, color="leiden")
 sc.pl.umap(adata, color="dataset")
 
 # %%
-sc.pl.umap(adata, color="dataset", groups=["Maynard_Bivona_2020_NSCLC"], size=8)
-
-# %%
-ah.plot_umap(adata, cmap="inferno", size=2)
-
-# %%
-sc.pl.umap(adata, color=["GPM6B", "ALB", "HBB"], cmap="inferno", size=2)
-
-# %%
-ah.plot_dotplot(adata)
+sc.pl.umap(adata, color="celltypist_prediction")
 
 # %%
 adata.var["mito"] = adata.var_names.str.lower().str.startswith("mt-")
@@ -68,33 +84,14 @@ sc.pp.calculate_qc_metrics(
     adata, qc_vars=("mito",), log1p=False, inplace=True, percent_top=None
 )
 
-# TODO: Re-enable this when we can use scANVI
-# # %% 
-# sc.pl.umap(
-#     adata, color="cell_type_predicted", legend_loc="on data", legend_fontoutline=2
-# )
+# %% 
+sc.pl.umap(
+    adata, color="cell_type_predicted", legend_loc="on data", legend_fontoutline=2
+)
 
 # %%
 sc.pl.umap(adata, color="leiden", legend_loc="on data", legend_fontoutline=2)
 
-# %%
-ct_map = {
-    "B cell": [8],
-    "Epithelial cell": [31, 18, 5, 13, 19, 12, 28, 23, 21, 29],
-    "Endothelial cell": [9, 25],
-    "Stromal": [14],
-    "Granulocytes": [22],
-    "Mast cell": [20],
-    "Myeloid": [0, 2, 30, 17, 27, 6, 11, 16],
-    "Plasma cell": [15],
-    "T cell": [7, 1, 4, 3, 10, 24],
-    "pDC": [26],
-    # they come from brain metastases
-    "Neuronal cells": [29],
-}
-
-# %%
-ah.annotate_cell_types(adata, ct_map)
 
 # %%
 adata.obs["cell_type"].value_counts()
